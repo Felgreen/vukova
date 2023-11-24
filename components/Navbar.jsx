88b66e492270b32b5872cd0281a10a0301f7f9a8
Enrique Green <quique.green92@gmail.com>
2023-11-24 12:19:38 -0300

    arreglos

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -106,14 +106,14 @@ const Navbar = () => {
             </li>
             <li
               className={
-                path === "/entrenamiento"
+                path === "/sedes"
                   ? `p-4 text-[#f37032] underline`
                   : `p-4 hover:text-[#f37032] text-[${textColor}] group transition-all duration-400 ease-in-out`
               }
             >
               <Link
                 className="bg-left-bottom bg-gradient-to-r from-[#f37032] to-[#f37032] bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
-                href="/entrenamiento"
+                href="/sedes"
               >
                 {t.nav.horarios}
               </Link>
--- pages/sedes/index.js
+++ pages/sedes/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const Sedes = () => {
+const Sedes = () => {
   return (
     <div>Sedes</div>
   )
 }
+
+export default Sedes
