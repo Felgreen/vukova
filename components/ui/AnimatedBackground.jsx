feff1338bdc2a2d0a4dd95d3db7c3ca74aa79b47
Enrique Green <quique.green92@gmail.com>
2023-11-24 17:01:13 -0300

    imagenes ticker

--- components/ui/AnimatedBackground.jsx
+++ components/ui/AnimatedBackground.jsx
@@ -1,7 +1,20 @@
 
 import Image from 'next/image'; 
+import { useRouter } from "next/router";
+import AnimatedTextWord from './AnimatedTextCharacter'; 
+import AnimatedTextCharacter from './AnimatedTextCharacter';
+
+import en from "../../public/locales/en/english.json";
+import es from "../../public/locales/es/espanol.json";
+
+
 
 const AnimatedBackground = () => {
+
+const router = useRouter();
+const { locale } = router;
+const t = locale === "es" ? es : en;
+
   return (
     <div section id='animatedbackground' className=' relative h-[130vh] w-screen bg-center bg-cover object-cover '>
         <div className='absolute inset-0'>
@@ -11,6 +24,10 @@ const AnimatedBackground = () => {
                 style={{objectFit:"cover"}}
                 fill
                  />
+                <div className='relative text-white'>
+                  <AnimatedTextWord  className="text-white" text={t.nosotros.title}/>
+                
+                </div>
         </div>
     </div>
     
