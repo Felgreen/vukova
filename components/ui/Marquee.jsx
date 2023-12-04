51f3ac1a7e310f4c00bb9df3129e0b1248b99b82
Enrique Green <quique.green92@gmail.com>
2023-12-04 13:57:46 -0300

    arreglos marquee para mobile y pantallas normales

--- components/ui/Marquee.jsx
+++ components/ui/Marquee.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// 1. Importing framer-motion
+
 import { motion } from "framer-motion";
 import { useRouter } from "next/router";
 
@@ -11,24 +11,37 @@ import es from "../../public/locales/es/espanol.json";
 // 2. Defining Variants
 const marqueeVariants = {
   animate: {
-    x: [50, -1035],
+    x: [50, -1400],
     transition: {
       x: {
-        repeat: Infinity,
-        repeatType: "loop",
-        duration: 7,
+        repeat:  Infinity,
+        repeatType: "reverse",
+        duration: 10,
         ease: "linear",
       },
     },
   },
 };
-
+const marqueeVariantsMobile = {
+  animate: {
+    x: [50, -2500],
+    transition: {
+      x: {
+        repeat:  Infinity,
+        repeatType: "reverse",
+        duration: 15,
+        ease: "linear",
+      },
+    },
+  },
+};
 const Marquee = () => {
 const router = useRouter();
 const { locale } = router;
 const t = locale === "es" ? es : en;
   return (
-    <div className="top-1/2 left-1/2 absolute transform -translate-x-1/2 ">
+    <>
+    <div className="top-1/2 left-1/2 absolute transform -translate-x-1/2 collapse md:visible">
       <div className={styles['marquee']}>
                 {/* 3. Using framer motion */}
         <motion.div
@@ -43,6 +56,22 @@ const t = locale === "es" ? es : en;
         </motion.div>
       </div>
     </div>
+    <div className="top-1/2 left-1/2 absolute transform -translate-x-1/2 md:collapse ">
+      <div className={styles['marquee']}>
+                {/* 3. Using framer motion */}
+        <motion.div
+          className={styles['track']}
+          variants={marqueeVariantsMobile}
+          animate="animate"
+        >
+          <h1>
+           {t.marquee[0]}
+          </h1>
+          
+        </motion.div>
+      </div>
+    </div>
+    </>
   );
 };
 
